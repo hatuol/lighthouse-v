d686491b9b1e35c64f17d567a6728e55a2ea0ad9
hatuol <delaemgryaz@gmail.com>
2020-11-19 22:05:57 +0300

    add more blocks

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,12 @@
-console.log('file 1');
+$(".block-about p").each(function(){
+  if ($(this).text().length > 300) {
+    $(this).text($(this).text().substring(0, 300) + "…");
+  }
+});
+
+$(".hot-news__about p").each(function(){
+    if ($(this).text().length > 1000) {
+      $(this).text($(this).text().substring(0, 1000) + "…");
+    }
+  });
+  
